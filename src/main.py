8114f981314a2147f985e4f9347cbc3f9671df83
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2021-06-10 14:23:08 -0300

    refactor: altera criação das cidades

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 from mapa import Mapa
 
 mapa = Mapa()
-mapa.cria_mapa()
 print(mapa.cidades[0].cidade.nome)
--- src/mapa.py
+++ src/mapa.py
@@ -4,7 +4,68 @@ from cidade import Cidade
 
 
 class Mapa:
+    arad = Cidade("Arad")
+    bucharest = Cidade("Bucharest")
+    craiova = Cidade("Craiova")
+    dobreta = Cidade("Dobreta")
+    eforie = Cidade("Eforie")
+    fagaras = Cidade("Fagaras")
+    giurgiu = Cidade("Giurgiu")
+    hirsova = Cidade("Hirsova")
+    iasi = Cidade("Iasi")
+    lugoj = Cidade("Lugoj")
+    mehadia = Cidade("Mehadia")
+    neamt = Cidade("Neamt")
+    oradea = Cidade("Oradea")
+    pitesti = Cidade("pitesti")
+    rimnicu_vilcea = Cidade("Rimnicu Vilcea")
+    sibiu = Cidade("Sibiu")
+    timisoara = Cidade("Timisoara")
+    urziceni = Cidade("Urziceni")
+    vaslui = Cidade("Vaslui")
+    zerind = Cidade("Zerind")
     cidades: List[CidadeMapa] = []
+
+    def __init__(self) -> None:
+        self.cidades.append(
+            CidadeMapa(self.arad, [self.zerind, self.sibiu, self.timisoara])
+        )
+        self.cidades.append(
+            CidadeMapa(
+                self.bucharest,
+                [self.giurgiu, self.urziceni, self.fagaras, self.pitesti],
+            )
+        )
+        self.cidades.append(
+            CidadeMapa(self.craiova, [self.pitesti, self.rimnicu_vilcea, self.dobreta])
+        )
+        self.cidades.append(CidadeMapa(self.dobreta, [self.craiova, self.mehadia]))
+        self.cidades.append(CidadeMapa(self.eforie, [self.hirsova]))
+        self.cidades.append(CidadeMapa(self.fagaras, [self.sibiu, self.bucharest]))
+        self.cidades.append(CidadeMapa(self.giurgiu, [self.bucharest]))
+        self.cidades.append(CidadeMapa(self.hirsova, [self.eforie, self.urziceni]))
+        self.cidades.append(CidadeMapa(self.iasi, [self.neamt, self.vaslui]))
+        self.cidades.append(CidadeMapa(self.lugoj, [self.mehadia, self.timisoara]))
+        self.cidades.append(CidadeMapa(self.mehadia, [self.lugoj, self.dobreta]))
+        self.cidades.append(CidadeMapa(self.neamt, [self.iasi]))
+        self.cidades.append(CidadeMapa(self.oradea, [self.zerind, self.sibiu]))
+        self.cidades.append(
+            CidadeMapa(
+                self.pitesti, [self.rimnicu_vilcea, self.craiova, self.bucharest]
+            )
+        )
+        self.cidades.append(
+            CidadeMapa(self.rimnicu_vilcea, [self.pitesti, self.craiova, self.sibiu])
+        )
+        self.cidades.append(
+            CidadeMapa(
+                self.sibiu, [self.fagaras, self.arad, self.oradea, self.rimnicu_vilcea]
+            )
+        )
+        self.cidades.append(CidadeMapa(self.timisoara, [self.lugoj, self.arad]))
+        self.cidades.append(CidadeMapa(self.urziceni, [self.hirsova, self.bucharest]))
+        self.cidades.append(CidadeMapa(self.vaslui, [self.iasi, self.urziceni]))
+        self.cidades.append(CidadeMapa(self.zerind, [self.oradea, self.arad]))
 
     def busca_profunda(partida: CidadeMapa, chegada: CidadeMapa) -> List[CidadeMapa]:
         return [partida, chegada]
@@ -22,53 +83,6 @@ class Mapa:
         partida: CidadeMapa, chegada: CidadeMapa
     ) -> List[CidadeMapa]:
         return [partida, chegada]
-
-    def cria_mapa(self) -> List[CidadeMapa]:
-        arad = Cidade("Arad")
-        bucharest = Cidade("Bucharest")
-        craiova = Cidade("Craiova")
-        dobreta = Cidade("Dobreta")
-        eforie = Cidade("Eforie")
-        fagaras = Cidade("Fagaras")
-        giurgiu = Cidade("Giurgiu")
-        hirsova = Cidade("Hirsova")
-        iasi = Cidade("Iasi")
-        lugoj = Cidade("Lugoj")
-        mehadia = Cidade("Mehadia")
-        neamt = Cidade("Neamt")
-        oradea = Cidade("Oradea")
-        pitesti = Cidade("pitesti")
-        rimnicu_vilcea = Cidade("Rimnicu Vilcea")
-        sibiu = Cidade("Sibiu")
-        timisoara = Cidade("Timisoara")
-        urziceni = Cidade("Urziceni")
-        vaslui = Cidade("Vaslui")
-        zerind = Cidade("Zerind")
-
-        self.cidades.append(CidadeMapa(arad, [zerind, sibiu, timisoara]))
-        self.cidades.append(
-            CidadeMapa(bucharest, [giurgiu, urziceni, fagaras, pitesti])
-        )
-        self.cidades.append(CidadeMapa(craiova, [pitesti, rimnicu_vilcea, dobreta]))
-        self.cidades.append(CidadeMapa(dobreta, [craiova, mehadia]))
-        self.cidades.append(CidadeMapa(eforie, [hirsova]))
-        self.cidades.append(CidadeMapa(fagaras, [sibiu, bucharest]))
-        self.cidades.append(CidadeMapa(giurgiu, [bucharest]))
-        self.cidades.append(CidadeMapa(hirsova, [eforie, urziceni]))
-        self.cidades.append(CidadeMapa(iasi, [neamt, vaslui]))
-        self.cidades.append(CidadeMapa(lugoj, [mehadia, timisoara]))
-        self.cidades.append(CidadeMapa(mehadia, [lugoj, dobreta]))
-        self.cidades.append(CidadeMapa(neamt, [iasi]))
-        self.cidades.append(CidadeMapa(oradea, [zerind, sibiu]))
-        self.cidades.append(CidadeMapa(pitesti, [rimnicu_vilcea, craiova, bucharest]))
-        self.cidades.append(CidadeMapa(rimnicu_vilcea, [pitesti, craiova, sibiu]))
-        self.cidades.append(CidadeMapa(sibiu, [fagaras, arad, oradea, rimnicu_vilcea]))
-        self.cidades.append(CidadeMapa(timisoara, [lugoj, arad]))
-        self.cidades.append(CidadeMapa(urziceni, [hirsova, bucharest]))
-        self.cidades.append(CidadeMapa(vaslui, [iasi, urziceni]))
-        self.cidades.append(CidadeMapa(zerind, [oradea, arad]))
-
-        return self.cidades
 
     def _limpa_busca(self):
         for cidade in self.cidades:
