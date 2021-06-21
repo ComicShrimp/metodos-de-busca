7d50bba68c32ca955fcc95b280a850ff786eaa95
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2021-06-20 22:18:55 -0300

    fix: conserta typo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from metodos_de_busca.buscas import BuscaGulosa, BuscaProfunda, BuscaInputDto
-from metodos_de_busca.mapa import Mapa
+from metodos_de_busca.sociedade.mapa import Mapa
 
 mapa = Mapa()
 print("Partida: " + mapa.partida.nome)
