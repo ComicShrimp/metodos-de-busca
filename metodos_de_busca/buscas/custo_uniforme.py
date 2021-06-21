40ea51d432e2f760fb4012fa5c6ffe92b78056ad
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2021-06-21 17:51:41 -0300

    wip: custo uniforme

--- metodos_de_busca/buscas/custo_uniforme.py
+++ metodos_de_busca/buscas/custo_uniforme.py
@@ -5,10 +5,56 @@ from metodos_de_busca.sociedade import Cidade
 from .busca import IBusca, BuscaInputDto, ResultadoBusca
 
 
+class Folha:
+    def __init__(
+        self, pilha: List[Cidade] = [], custo_total: int = 0, finalizou: bool = False
+    ) -> None:
+        self.pilha = pilha
+        self.custo_total = custo_total
+        self.finalizou = finalizou
+
+
 class BuscaDeCustoUniforme(IBusca):  # A*
     def __init__(self):
-        self.pilha: List[Cidade] = []
+        self.folhas: List[Folha] = []
 
     def executa(self, input_dto: BuscaInputDto) -> ResultadoBusca:
-        # TODO: implementar
-        pass
+        if self.folhas == []:
+            self.folhas.append(
+                Folha(
+                    pilha=[input_dto.partida],
+                    custo_total=input_dto.partida.custo,
+                    finalizou=False,
+                )
+            )
+
+        folha_menor_custo = self.achar_folha_menor_custo()
+        self.folhas.remove(folha_menor_custo)
+        folha_menor_custo.pilha[-1].visitar()
+
+        for cidade in folha_menor_custo.pilha[-1].vizinhos:
+            if not cidade.cidade_destino.foi_visitado():
+                custo_total = cidade.custo_do_caminho + cidade.cidade_destino.custo
+
+                destino = False
+                if cidade.cidade_destino.nome == input_dto.chegada.nome:
+                    destino = True
+
+                self.folhas.append(
+                    Folha(
+                        pilha=(folha_menor_custo.pilha + [cidade.cidade_destino]),
+                        custo_total=custo_total,
+                        finalizou=destino,
+                    )
+                )
+
+        return ResultadoBusca(caminho_nao_encontrado=True)
+
+    def achar_folha_menor_custo(self) -> Folha:
+        menor_custo = self.folhas[0]
+
+        for folha in self.folhas:
+            if folha.custo_total < menor_custo.custo_total:
+                menor_custo = folha
+
+        return menor_custo
